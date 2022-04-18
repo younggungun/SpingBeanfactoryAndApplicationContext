d0db17cde529628d59a0a10dd3bd1946127d4118
Dayoung <395496298@qq.com>
2022-04-18 11:58:19 +0800

    1-2 利用反射查看DefaultSingletonBeanRegistry中的singletonObjects单例池

--- src/main/java/com/example/spingbeanfactoryandapplicationcontext/SpingBeanfactoryAndApplicationContextApplication.java
+++ src/main/java/com/example/spingbeanfactoryandapplicationcontext/SpingBeanfactoryAndApplicationContextApplication.java
@@ -1,13 +1,28 @@
 package com.example.spingbeanfactoryandapplicationcontext;
 
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpingBeanfactoryAndApplicationContextApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpingBeanfactoryAndApplicationContextApplication.class, args);
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        //ctrl+alt+u 查看类图
+        ConfigurableApplicationContext run = SpringApplication.run(SpingBeanfactoryAndApplicationContextApplication.class, args);
+
+        Field defaultSingletonBeanRegistry = DefaultSingletonBeanRegistry.class.getDeclaredField("singletonObjects");
+        defaultSingletonBeanRegistry.setAccessible(true);
+        ConfigurableListableBeanFactory beanFactory = run.getBeanFactory();
+        Map<String,Object> map = (Map<String, Object>) defaultSingletonBeanRegistry.get(beanFactory);
+        map.forEach((k,v) ->
+            System.out.println("key:"+k+">>>>>>>>>>"+"value:"+v)
+        );
     }
 
 }
