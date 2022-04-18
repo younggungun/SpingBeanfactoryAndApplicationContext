7e33b348f1a2b973c42fbeebeff88aa3abd71c6a
Dayoung <395496298@qq.com>
2022-04-18 10:31:39 +0800

    1-1创建简单的springboot工程

---
+++ src/main/java/com/example/spingbeanfactoryandapplicationcontext/SpingBeanfactoryAndApplicationContextApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbeanfactoryandapplicationcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBeanfactoryAndApplicationContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBeanfactoryAndApplicationContextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spingbeanfactoryandapplicationcontext/SpingBeanfactoryAndApplicationContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingbeanfactoryandapplicationcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBeanfactoryAndApplicationContextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
